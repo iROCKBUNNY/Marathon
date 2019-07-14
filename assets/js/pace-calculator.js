657df3341197672143f75cd4bef9f30e0afea388
iROCKBUNNY <irockbunny@outlook.com>
2019-07-15 00:53:06 +0800

    Update

--- assets/js/pace-calculator.js
+++ assets/js/pace-calculator.js
@@ -50,11 +50,11 @@ $('#pace-calculator-form').form({
 
 function calculatePace() {
     if ($('#pace-calculator-form').form('validate form')) {
-        var values = $('#pace-calculator-form').form('get values');
-        var pace = ((Number(values.hour) * 60 + Number(values.minute)) * 60 + Number(values.second)) / Number(values.distance);
-        var minutes = Math.floor(pace / 60);
-        var seconds = Math.floor(pace - minutes * 60);
-        var milliseconds = (pace - Math.floor(pace)) * 1000;
+        let values = $('#pace-calculator-form').form('get values');
+        let pace = ((Number(values.hour) * 60 + Number(values.minute)) * 60 + Number(values.second)) / Number(values.distance);
+        let minutes = Math.floor(pace / 60);
+        let seconds = Math.floor(pace - minutes * 60);
+        let milliseconds = (pace - Math.floor(pace)) * 1000;
         $('#pace').html(`${minutes.toString().padStart(2, '0')}&prime;${seconds.toString().padStart(2, '0')}&Prime;<span class="ui mini text">${milliseconds.toFixed(0).padStart(3, '0')}</span>`);
     };
 };
